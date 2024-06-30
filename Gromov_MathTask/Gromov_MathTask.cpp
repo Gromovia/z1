bc0bb4c14e59c2a7e719be7e353b0e6b8891b5fc
Gromovia <164464132+Gromovia@users.noreply.github.com>
2024-06-30 18:56:13 +0300

    add files

---
+++ Gromov_MathTask/Gromov_MathTask.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+bool UserInput(string input) {
+
+
+	if (input.empty()) return false;
+
+	try {
+		int number = stoi(input);
+	}
+	catch (...) {
+		return false;
+	}
+	if (stoi(input) < 0)
+	{
+		return false;
+	}
+	return true;
+
+	
+}
+int CalcRectangleArea(int NumberA, int NumberB) {
+	return NumberA * NumberB + 10;
+}
+int main() {
+
+
+	string str_input;
+	cout << "input height A" << endl;
+	getline(cin, str_input);
+	while (!UserInput(str_input)) {
+		cout << "input height A" << endl;
+		getline(cin, str_input);
+	}
+	int NumberA = stoi(str_input);
+	cout << "input weight B" << endl;
+	getline(cin, str_input);
+	while (!UserInput(str_input)) {
+		cout << "input weight B" << endl;
+		getline(cin, str_input);
+	}
+	int NumberB = stoi(str_input);
+	int RectangleArea = CalcRectangleArea(NumberA, NumberB);
+	cout << "Area of Rectangle is" << RectangleArea << endl;
+}
---
+++ Gromov_MathTaskClassLibraryTests,/Gromov_MathTaskClassLibraryTests,.cpp
@@ -0,0 +1,62 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Gromov_MathTask/Gromov_MathTask.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GromovMathTaskClassLibraryTests
+{
+	TEST_CLASS(GromovMathTaskTests)
+	
+	
+	{
+	public:
+		
+		TEST_METHOD(RectangleArea_3and5_15returned) {
+
+			int a = 3;
+			int b = 5;
+
+			int expected = 25;
+
+			int actual = CalcRectangleArea(a, b);
+
+			Assert::AreEqual(expected, actual, L"Incorrect value area");
+		}
+		//тест, проверяющий отсев пустых значений
+		TEST_METHOD(Check_UserInput_Empty) {
+
+			// исходные данные
+			string str = "";
+			// ожидаемое значение результата работы функции UserInput
+			bool expected = false;
+			// получение значения с помощью тестируемого метода
+			bool actual = UserInput(str);
+			// сравнение ожидаемого результата с полученным
+			Assert::AreEqual(expected, actual, L"Entered empty value");
+		}
+
+		//тест, проверяющий отсев нецифровых символов
+		TEST_METHOD(Check_UserInput_Letter) {
+			// исходные данные
+			string str = "a";
+			// ожидаемое значение результата работы функции UserInput
+			bool expected = false;
+			// получение значения с помощью тестируемого метода
+			bool actual = UserInput(str);
+			// сравнение ожидаемого результата с полученным
+			Assert::AreEqual(expected, actual, L"Entered empty value");
+		}
+		//тест, проверяющий отсев отрицательных значений
+		TEST_METHOD(Check_UserInput_NegativeValue) {
+
+			// исходные данные
+			string str = "-5";
+			// ожидаемое значение результата работы функции UserInput
+			bool expected = false;
+			// получение значения с помощью тестируемого метода
+			bool actual = UserInput(str);
+			// сравнение ожидаемого результата с полученным
+			Assert::AreEqual(expected, actual, L"Entered empty value");
+		}
+	};
+}
